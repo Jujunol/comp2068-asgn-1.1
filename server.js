d9e08b75871205b0de9b920ecf31f81fd24ff0b0
Jujunol <jujunol@outlook.com>
2016-02-04 02:21:21 +0000

    Changed site structure and other things

--- server.js
+++ server.js
@@ -1,3 +1,8 @@
+// - Filename: Pretty sure you can read it in the title bar
+// - Author's Name: John Horne
+// - Website Name: comp2068-asgn-1.1
+// - File Description: Runs the server 
+
 var express = require("express");
 
 var app = express();
@@ -6,12 +11,23 @@ var port = process.env.port || 8080;
 app.set('views', './views');
 app.set('view engine', 'jade');
 
-app.use('/lib', express.static(__dirname + "/lib"));
-app.use('/lib', express.static(__dirname + "/scripts"));
+app.use('/lib', express.static(__dirname + "/public/lib"));
+app.use('/lib', express.static(__dirname + "/public/scripts"));
 
+
+// Main navigation
 app.get('/', function(req, res) {
     res.render('index');
+}).get('/projects', function(req, res) {
+    res.render('projects');
+}).get('/services', function(req, res) {
+    res.render('services');
+}).get('/about', function(req, res) {
+    res.render('about');
+}).get('/contact', function(req, res) {
+    res.render('contact');
 });
+
 
 app.listen(port, function() {
     console.log("Server running on " + port);
